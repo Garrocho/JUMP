4b603fcb0c49715fcdf6e009a626cf1623ba74e7
CharlesGarrocho <charles.garrocho@gmail.com>
2013-10-11 10:02:57 -0300

    adicionado: módulo para configuração inicial da tela e iniciação do menu do jogo. #2

---
+++ start.py
@@ -0,0 +1,14 @@
+import pygame
+from gamelib import menu
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.mouse.set_visible(0)
+    pygame.display.set_caption("Jump")
+    size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
+    screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+    #screen = pygame.display.set_mode((740, 580))
+    m = menu.Menu(screen)
+    m.loop()
+	
