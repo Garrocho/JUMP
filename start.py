99a447e9e031fb463b25683f96b32aef952183c7
CharlesGarrocho <charles.garrocho@gmail.com>
2013-10-12 22:28:21 -0300

    alterado: descrição do título da aplicação.

--- start.py
+++ start.py
@@ -5,7 +5,7 @@ from gamelib import menu
 if __name__ == '__main__':
     pygame.init()
     pygame.mouse.set_visible(0)
-    pygame.display.set_caption("Jump")
+    pygame.display.set_caption("JUMP!")
     size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
     screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
     #screen = pygame.display.set_mode((740, 580))
