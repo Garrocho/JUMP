ca8a11c50fd998c7e7ca6d84551f3740d78676af
CharlesGarrocho <charles.garrocho@gmail.com>
2013-10-16 10:50:57 -0300

    removido: espaço dentro do abre e fecha parênteses.

--- gamelib/engine.py
+++ gamelib/engine.py
@@ -9,19 +9,19 @@ class Jogo:
     screen_size = None
     run         = True
     
-    def __init__( self, screen ):
+    def __init__(self, screen):
         self.screen      = screen
         self.screen_size = self.screen.get_size()
         self.carrega_dados()
 
-    def carrega_dados( self ):
+    def carrega_dados(self):
         # carregando dados
         #media.executar_musica("musica_inicio_do_jogo.ogg", 0.75)
         self.bg_game = media.carrega_imagem_menu('jogo_background_1.jpg')
         self.img_louco = media.carrega_imagem_menu('louco.png')
 
         # Carregando Atores
-        posicao      = [ self.screen_size[ 0 ] / 2, self.screen_size[ 1 ] - 200 ]
+        posicao      = [self.screen_size[ 0 ] / 2, self.screen_size[ 1 ] - 200]
         self.jogador = atores.Jogador(imagem=self.img_louco, posicao=posicao)
 
         # Lista de Atores
@@ -29,7 +29,7 @@ class Jogo:
             "jogador" : pygame.sprite.RenderPlain(self.jogador)
         }
 
-    def tratador_eventos( self ):
+    def tratador_eventos(self):
         for event in pygame.event.get():
             t = event.type
             if t in ( KEYDOWN, KEYUP ):
@@ -54,16 +54,16 @@ class Jogo:
         for ator in self.lista_atores.values():
             ator.draw(self.screen)
 
-    def acao_atores( self ):
+    def acao_atores(self):
         # Verifica se personagem foi atingido por um tiro, se trombou em algum inimigo
         # se atingiu algum alvo. Aumenta a experiencia baseado no numero de acertos, etc
         pass
 
-    def administrar( self ):
+    def administrar(self):
         # Fazer inimigos executar alguma acao, modificar level do jogo, etc.
         pass
 
-    def loop( self ):
+    def loop(self):
 
         while self.run:
 
