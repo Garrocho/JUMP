c4b9bc2656f9b19ac9abfba6ef84b7248f73350f
CharlesGarrocho <charles.garrocho@gmail.com>
2013-10-17 10:17:45 -0300

    adicionado: classe Fogo que extende Sprite. #6

--- gamelib/atores.py
+++ gamelib/atores.py
@@ -31,3 +31,35 @@ class Jogador(pygame.sprite.Sprite):
                 self.pulando = False
                 self.fases_pulo	 = 0
                 self.image = self.fatias[0]
+
+
+class Fogo(pygame.sprite.Sprite):
+
+    def __init__(self, imagem, posicao):
+        pygame.sprite.Sprite.__init__(self)
+        self.image       = imagem[0]
+        self.fatias      = imagem
+        self.rect        = self.image.get_rect()
+        self.rect.center = (posicao)
+        self.fatia_atual = 0
+        self.soma        = True
+        self.fatia_tam   = len(self.fatias)-1
+        self.tempo_fatia = 10
+
+    def update(self):
+        if self.tempo_fatia == 0:
+            if self.fatia_atual < self.fatia_tam and self.soma is True:
+                self.tempo_fatia = 15
+                self.fatia_atual = self.fatia_atual+1
+                if self.fatia_atual == self.fatia_tam:
+                    self.soma = False
+                    self.tempo_fatia = 40
+            else:
+                self.tempo_fatia = 20
+                self.fatia_atual = self.fatia_atual-1
+                if self.fatia_atual == 0:
+                    self.soma = True
+                    self.tempo_fatia = 100
+            self.image = self.fatias[self.fatia_atual]
+        else:
+            self.tempo_fatia = self.tempo_fatia-1
