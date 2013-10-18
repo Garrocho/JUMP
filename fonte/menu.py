d801cb2e87cd393ba925578fef481e7ac682abb0
CharlesGarrocho <charles.garrocho@gmail.com>
2013-10-18 10:08:35 -0300

    alterado: nome das função do menu.

--- fonte/menu.py
+++ fonte/menu.py
@@ -1,38 +1,38 @@
 import sys
-import media
-import engine
+import dados
+import motor
 import pygame
 from pygame.locals import *
 
 
-def novoJogo(screen):
-    jogo = engine.Jogo(screen)
+def novo_jogo(screen):
+    jogo = motor.Jogo(screen)
     jogo.loop()
 
 
 class Menu(object):    
 
     def __init__(self, screen):
-        media.executar_musica("menu_loop.wav", 0.75)
+        dados.executar_musica("menu_loop.wav", 0.75)
         self.screen = screen
-        self.menu = NFMenu(["Novo Jogo", lambda: novoJogo(screen)], ["Sair", sys.exit])
+        self.menu = NFMenu(["Novo Jogo", lambda: novo_jogo(screen)], ["Sair", sys.exit])
         self.clock = pygame.time.Clock()
         events = pygame.event.get()
-        self.menu.update(events)
-        self.menu.draw(self.screen)
-        self.bg = media.carrega_imagem_menu('menu_background.jpg')
-        self.fonteGrande = pygame.font.Font(media.carrega_fonte("BLADRMF_.TTF"), 120)
+        self.menu.atualizar(events)
+        self.menu.desenhar(self.screen)
+        self.bg = dados.carrega_imagem_menu('menu_background.jpg')
+        self.fonteGrande = pygame.font.Font(dados.carrega_fonte("BLADRMF_.TTF"), 120)
         self.run = True
 
     def loop(self):
         while self.run:
             self.clock.tick(40)
             events = pygame.event.get()
-            self.menu.update(events)
+            self.menu.atualizar(events)
             self.screen.blit(self.bg, (0, 0))
             ren = self.fonteGrande.render("JUMP!", 1, (255, 255, 255))
             self.screen.blit(ren, (250-ren.get_width()/2, 180))
-            self.menu.draw(self.screen)
+            self.menu.desenhar(self.screen)
             pygame.display.flip()
 
 
@@ -41,9 +41,9 @@ class NFMenu:
     def __init__(self, *vetor_funcoes_menu):
 
         self.vetor_funcoes_menu = vetor_funcoes_menu
-        self.som_menu_item = media.obter_som('menu_item.wav')
+        self.som_menu_item = dados.obter_som('menu_item.wav')
         self.hcolor = (255, 0, 0)
-        self.fonte = pygame.font.Font(media.carrega_fonte("GOODTIME.ttf"), 50)
+        self.fonte = pygame.font.Font(dados.carrega_fonte("GOODTIME.ttf"), 50)
         self.posicao_atual = 0
         self.width = 1
         self.color = [255, 255, 255]
@@ -57,7 +57,7 @@ class NFMenu:
         self.x = 250-(self.width/2)
         self.y = 450-(self.height/2)
 
-    def draw(self, surface):
+    def desenhar(self, surface):
         i=0
         for funcao in self.vetor_funcoes_menu:
             if i==self.posicao_atual:
@@ -71,7 +71,7 @@ class NFMenu:
             surface.blit(ren, ((self.x+self.width/2) - ren.get_width()/2, self.y + i*(self.fonte.get_height()+4)))
             i+=1
             
-    def update(self, events):
+    def atualizar(self, events):
         for e in events:
             if e.type == pygame.KEYDOWN:
                 if e.key == pygame.K_DOWN:
