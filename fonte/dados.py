ba54f1dec552e6990fa93f4ce2dc5cc1e18e40ed
CharlesGarrocho <charles.garrocho@gmail.com>
2013-10-23 11:36:58 -0200

    adicionado: método para cortar colunas. #9

--- fonte/dados.py
+++ fonte/dados.py
@@ -70,10 +70,13 @@ def obter_som(nome_arquivo, volume=3.0):
 
 
 def carrega_imagem_fatias(w, h, nome_arquivo):
-    fatias = []
-    imagem_mestre = pygame.image.load(carrega('imagens', nome_arquivo)).convert_alpha()
-    width_mestre, height_mestre = imagem_mestre.get_size()
-    for i in xrange(int(width_mestre / w)):
-        # Recorda varias partes da imagem
-        fatias.append(imagem_mestre.subsurface((i * w, 0, w, h)))
-    return fatias
+    imagens = []
+    master_image = pygame.image.load(carrega('imagens', nome_arquivo)).convert_alpha()
+    mestre_w, mestre_h = master_image.get_size()
+    colunas = mestre_w / w
+    linhas = mestre_h / h
+    for i in xrange (linhas):
+        for j in xrange (colunas):
+            pequeno_sprite = master_image.subsurface((j*w,i*h,w,h))
+            imagens.append(pequeno_sprite)
+    return imagens
