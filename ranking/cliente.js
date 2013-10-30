baff57287ae21d49c00f041b520d88b5553b4ea3
CharlesGarrocho <charles.garrocho@gmail.com>
2013-10-30 10:31:50 -0200

    adicionado: função para assim que carregar a página chamar as funções set eventos e conexão. #12

---
+++ ranking/cliente.js
@@ -0,0 +1,9 @@
+// criamos uma variável 'conexao'
+var conexao = null;
+ 
+// depois do carregamento da página, chamamos 'setConexao' e 'setEventos'
+window.onload = function () {
+    setConexao();
+	setEventos();
+}
+ 
