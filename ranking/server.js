b4a2bcfdc418897900efe7df0dfd3b709ab78093
CharlesGarrocho <charles.garrocho@gmail.com>
2013-10-30 10:34:29 -0200

    adicionado: servidor de arquivos. #12

--- ranking/server.js
+++ ranking/server.js
@@ -16,3 +16,22 @@ var url = require("url");
 // vai entender as requisições GET que fizermos
 var st = require('node-static');
  
+// criamos um servidor de arquivos...
+var fileServer = new st.Server();
+ 
+// criamos um servidor de arquivos usando um método da variável 'http'
+// esse método recebe um callback com 'request' e 'response' como argumentos
+var servidor = http.createServer(function(request, response) {
+ 
+    // adicionamos um evento a request, ou seja quando tivermos 'end',
+    // no final da requisição, o callback que passamos como segundo parâmetro
+    // será executado...
+    request.addListener('end', function () {
+        // adicionamos um console para ver toda a requisição para uma url específica
+        console.log(request.url);
+ 
+        // chamamos o método 'serve' de 'fileServer' passando 'request' e 'response' como argumento
+        fileServer.serve(request, response);
+    });
+});
+ 
