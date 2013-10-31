a4af8e0d711992b62839d759a857aaf6b883a1d6
CharlesGarrocho <charles.garrocho@gmail.com>
2013-10-31 17:33:01 -0200

    adicionado: módulo de edição de texto. #11

--- fonte/editor.py
+++ fonte/editor.py
@@ -6,30 +6,47 @@ from pygame.locals import *
 
 class Editor:
 	ALFABETO = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-	nome = "Entre com Seu Nome: "
+	nome = ""
 
 	def __init__(self, screen):
 		self.screen = screen
-		self.fonte = pygame.font.Font(dados.carrega_fonte("GOODTIME.ttf"), 25)
+		self.fonte_grande = pygame.font.Font(dados.carrega_fonte("BLADRMF_.TTF"), 120)
+        self.fonte_menor = pygame.font.Font(dados.carrega_fonte("GOODTIME.ttf"), 25)
 
 	def tratador_eventos(self):
 		for evento in pygame.event.get():
 			tipo = evento.type
 			if tipo == KEYDOWN:
 				chave = evento.key-97
-				if chave == -70:
-					self.sair = True
-				elif chave < len(self.ALFABETO) and chave >= 0:
-					self.nome+= self.ALFABETO[chave]
+				if chave < len(self.ALFABETO) and chave >= 0:
+					self.nome += self.ALFABETO[chave]
 				elif chave == -89:
-					#volta
+					self.nome = self.nome[:-1]
+				elif chave == -70:
+					self.sair = True
+				elif chave == -65:
+					self.nome += " "
 				elif chave == -84:
-					#concluido
+					self.sair = False
 
 	def desenhar(self):
-		rend = self.fonte.render(self.nome, 1, (80, 100, 250))
+		ren_maior = self.fonte_grande.render("Game Over", 1, (255, 255, 255))
+		ren_menor = self.fonte_menor.render("Pressione ESC Para Voltar ao Menu do Jogo!", 1, (80, 100, 250))
+		ren_pontua = self.fonte_menor.render("Distancia Percorrida: % 4d" % self.jogador.status["Distancia"], 1, (50, 100, 50))
+		ren_moedas = self.fonte_menor.render("Moedas Conquistadas: % 4d" % self.jogador.status["Moedas"], 1, (50, 100, 50))
+		text_rect = ren_maior.get_rect()
+		text_x = self.screen.get_width()/2 - text_rect.width/2
+		text_y = self.screen.get_height()/5 - text_rect.height/5
 		self.screen.fill((0,0,0))
-		self.screen.blit(rend, [10, 10])
+		self.screen.blit(ren_maior, [text_x, text_y])
+		self.screen.blit(ren_pontua, [text_x, text_y+150])
+		self.screen.blit(ren_moedas, [text_x, text_y+200])
+		self.screen.blit(ren_menor, [text_x+10, text_y+500])
+		pygame.display.flip()
+
+		rend = self.fonte_menor.render("Entre com Seu Nome: " + self.nome, 1, (80, 100, 10))
+		self.screen.fill((0,0,0))
+		self.screen.blit(rend, [text_x, text_y+100])
 		pygame.display.flip()
 
 	def loop(self):
@@ -46,3 +63,4 @@ if __name__ == '__main__':
 	screen = pygame.display.set_mode((900, 400))
 	editor = Editor(screen)
 	editor.loop()
+	pygame.quit()
