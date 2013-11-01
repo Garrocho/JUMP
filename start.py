de6cbd7c713a5ebdbee7a3e42d84b8c77b60545a
CharlesGarrocho <charles.garrocho@gmail.com>
2013-11-01 11:03:54 -0200

    adicionado: começo de implementação de inicialização de serviços.

--- start.py
+++ start.py
@@ -1,7 +1,13 @@
 import pygame
+import subprocess
 from fonte import menu
+from fonte import dados
+import subprocess
+
+#subprocess.call(["node", dados.ranking_dir + "/static/js/servidor.js"])
+#subprocess.call(["python -m SimpleHTTPServer", dados.ranking_dir])
 
 
 if __name__ == '__main__':
-    m = menu.Menu()
-    m.loop()
+	m = menu.Menu()
+	m.loop()
