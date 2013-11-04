a4d8285a8a1f5d77a6ef4433f4a85c3436465eb7
CharlesGarrocho <charles.garrocho@gmail.com>
2013-11-04 09:28:29 -0200

    adicionado: verificação do ranking antes de processa-lo.

--- fonte/menu.py
+++ fonte/menu.py
@@ -11,9 +11,9 @@ class Menu(object):
         pygame.init()
         pygame.mouse.set_visible(0)
         pygame.display.set_caption("JUMP!")
-        size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-        screen = pygame.display.set_mode((1024, 768), pygame.FULLSCREEN)
-        #screen = pygame.display.set_mode((1024, 768))
+        #size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
+        #screen = pygame.display.set_mode((1024, 768), pygame.FULLSCREEN)
+        screen = pygame.display.set_mode((1024, 768))
         dados.executar_musica("menu.ogg", 1.5)
         self.som_menu_item = dados.obter_som('menu_item.ogg')
         self.screen = screen
@@ -99,20 +99,21 @@ class Menu(object):
         self.screen.blit(ren, [850, 100])
         cont = 1
         y = 140
-        
-        for i in rank_json:
-            ren = self.fonte.render(str(cont), 1, self.hcor)
-            self.screen.blit(ren, [10, y])
-            ren = self.fonte.render(i['nome'], 1, self.hcor)
-            self.screen.blit(ren, [155, y])
-            ren = self.fonte.render(i['distancia'], 1, self.hcor)
-            self.screen.blit(ren, [555, y])
-            ren = self.fonte.render(i['moedas'], 1, self.hcor)
-            self.screen.blit(ren, [855, y])
-            cont += 1
-            y += 40
-            if cont == 16:
-                break
+
+        if rank_json is not None:
+            for i in rank_json:
+                ren = self.fonte.render(str(cont), 1, self.hcor)
+                self.screen.blit(ren, [10, y])
+                ren = self.fonte.render(i['nome'], 1, self.hcor)
+                self.screen.blit(ren, [155, y])
+                ren = self.fonte.render(i['distancia'], 1, self.hcor)
+                self.screen.blit(ren, [555, y])
+                ren = self.fonte.render(i['moedas'], 1, self.hcor)
+                self.screen.blit(ren, [855, y])
+                cont += 1
+                y += 40
+                if cont == 16:
+                    break
 
         ren = self.fonte.render('Pressione ESC para Retornar ao menu principal', 1, self.cor)
         self.screen.blit(ren, [self.pos_central, self.screen.get_height() - 35])
