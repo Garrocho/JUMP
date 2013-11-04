d775539fa84d4fc5243ae7a5f401617ea02f9145
CharlesGarrocho <charles.garrocho@gmail.com>
2013-11-04 09:26:06 -0200

    adicionado: função para desenhar os 15 pmelhores distancias.

--- fonte/menu.py
+++ fonte/menu.py
@@ -2,7 +2,6 @@ import sys
 import dados
 import motor
 import pygame
-import webbrowser
 from pygame.locals import *
 
 
@@ -12,9 +11,9 @@ class Menu(object):
         pygame.init()
         pygame.mouse.set_visible(0)
         pygame.display.set_caption("JUMP!")
-        # size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-        # screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
-        screen = pygame.display.set_mode((1024, 768))
+        size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
+        screen = pygame.display.set_mode((1024, 768), pygame.FULLSCREEN)
+        #screen = pygame.display.set_mode((1024, 768))
         dados.executar_musica("menu.ogg", 1.5)
         self.som_menu_item = dados.obter_som('menu_item.ogg')
         self.screen = screen
@@ -53,7 +52,7 @@ class Menu(object):
 
     def desenhar(self):
         self.screen.fill((0,0,0))
-        ren_maior = self.fonte_grande.render("JUMP!", 1, (255, 255, 255))
+        ren_maior = self.fonte_grande.render("JUMP!", 1, self.hcor)
         self.pos_central = (self.screen.get_width() - ren_maior.get_rect().width)/2
         self.screen.blit(ren_maior, [self.pos_central, 100])
         
@@ -74,8 +73,54 @@ class Menu(object):
             jogo = motor.Jogo(self.screen)
             jogo.loop()
         elif self.posicao_atual == 2:
-            webbrowser.open('http://127.0.0.1:8000', new=0, autoraise=True)
+            self.ranking()
         elif self.posicao_atual == 3:
             pass
         else:
             self.sair = True
+
+    def ranking(self):
+        rank_json = dados.obter_ranking()
+
+        self.screen.fill((0,0,0))
+        self.fonte_g = pygame.font.Font(dados.carrega_fonte("BLADRMF_.TTF"), 92)
+        ren_maior = self.fonte_g.render("Ranking do JUMP!", 1, self.hcor)
+        self.pos_central = (self.screen.get_width() - ren_maior.get_rect().width)/2
+        self.screen.blit(ren_maior, [self.pos_central, 5])
+
+        self.fonte = pygame.font.Font(dados.carrega_fonte("GOODTIME.ttf"), 30)
+        ren = self.fonte.render('Rank', 1, self.cor)
+        self.screen.blit(ren, [5, 100])
+        ren = self.fonte.render('Jogador', 1, self.cor)
+        self.screen.blit(ren, [150, 100])
+        ren = self.fonte.render('Distancia', 1, self.cor)
+        self.screen.blit(ren, [550, 100])
+        ren = self.fonte.render('Moedas', 1, self.cor)
+        self.screen.blit(ren, [850, 100])
+        cont = 1
+        y = 140
+        
+        for i in rank_json:
+            ren = self.fonte.render(str(cont), 1, self.hcor)
+            self.screen.blit(ren, [10, y])
+            ren = self.fonte.render(i['nome'], 1, self.hcor)
+            self.screen.blit(ren, [155, y])
+            ren = self.fonte.render(i['distancia'], 1, self.hcor)
+            self.screen.blit(ren, [555, y])
+            ren = self.fonte.render(i['moedas'], 1, self.hcor)
+            self.screen.blit(ren, [855, y])
+            cont += 1
+            y += 40
+            if cont == 16:
+                break
+
+        ren = self.fonte.render('Pressione ESC para Retornar ao menu principal', 1, self.cor)
+        self.screen.blit(ren, [self.pos_central, self.screen.get_height() - 35])
+
+        pygame.display.flip()
+        sair = False
+        while not sair:
+            eventos = pygame.event.get()
+            for e in eventos:
+                if e.type == KEYDOWN and e.key == K_ESCAPE:
+                    sair = True
