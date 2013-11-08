a7bd0d617b8ca3ecafe657713b41d41b81884371
paulovf <paulovfrancisco@gmail.com>
2013-11-08 10:01:53 -0200

    alterado: remoção de bug no aqruivo cliente.js

--- ranking/static/js/cliente.js
+++ ranking/static/js/cliente.js
@@ -43,14 +43,19 @@ var setConexao = function () {
         }
         if(json != null){
             for (i=0; i < json.length; i++){
-                $("table#tabela tbody")
-                if(json[i]["cidade"] == 'None' || json[i]["cidade"] == ''):
+                if(json[i]["cidade"] == 'None' || json[i]["cidade"] == ''){
+                    $("table#tabela tbody")
                     .append('<tr><th>' + (i+1) + '</th><td>' + json[i]["nome"] + ' </td><td>' + json[i]["distancia"] + '</td><td>' + json[i]["moedas"] + '</td><td> - </td></tr>')
-                else:
+                    .closest("table#tabela")
+                    //.table("refresh")
+                    .trigger("create");
+                }else{
+                    $("table#tabela tbody")
                     .append('<tr><th>' + (i+1) + '</th><td>' + json[i]["nome"] + ' </td><td>' + json[i]["distancia"] + '</td><td>' + json[i]["moedas"] + '</td><td><a href="http://localhost:8000/localizacao.html?jogador=' + json[i]["nome"] + '&localizacao=' + json[i]["cidade"] + '">' + json[i]["cidade"] + '</a></td></tr>')
-                .closest("table#tabela")
-                //.table("refresh")
-                .trigger("create");   
+                    .closest("table#tabela")
+                    //.table("refresh")
+                    .trigger("create");
+                }
             }         
         }
     };
