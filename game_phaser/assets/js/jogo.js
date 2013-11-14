df7702088cc6b92d844b98e9b0a082006fe27254
CharlesGarrocho <charles.garrocho@gmail.com>
2013-11-14 17:36:28 -0200

    adicionado: administração de moedas randômicas.

--- game_phaser/assets/js/jogo.js
+++ game_phaser/assets/js/jogo.js
@@ -1,5 +1,5 @@
-var game = new Phaser.Game(1024, 645, Phaser.AUTO, '', { preload: preload, create: create, update: update});
-var fundo1, fundo2;
+var game = new Phaser.Game(1024, 768, Phaser.AUTO, '', { preload: preload, create: create, update: update});
+var fundo1, fundo2, fundo3;
 var corredor;
 var texto;
 var grupo;
@@ -7,28 +7,34 @@ var contador;
 var normal;
 var inimigos = new Array();
 var moedas = new Array();
+var qtde_moedas;
+var som_moeda, som_pulo;
+var pos_atual;
 
 function preload() {
      game.load.image('nuvem', 'assets/img/nuvem.png');
      game.load.image('montanha', 'assets/img/montanha.png');
+     game.load.image('caminho', 'assets/img/caminho.png');
      game.load.spritesheet('moeda', 'assets/img/moedas.png', 44, 40, 10);
      game.load.spritesheet('correndo', 'assets/img/correndo.png', 75, 132, 14);
      game.load.spritesheet('inimigo', 'assets/img/inimigo.png', 75, 65, 4);
      game.load.spritesheet('jogar', 'assets/img/botao_jogar.png', 106, 42, 2);
      game.load.audio('musica', ['assets/sons/fase_1.wav']);
+     game.load.audio('moeda', ['assets/sons/moeda.wav']);
+     game.load.audio('pulo', ['assets/sons/pulo.ogg']);
 }
 
 function create() {
     fundo1 = game.add.tileSprite(0, 0, 1024, 512, 'nuvem');
-    fundo2 = game.add.tileSprite(0, 200, 1024, 512, 'montanha');
-    corredor = game.add.sprite(250, 10, 'correndo');
+    fundo2 = game.add.tileSprite(0, 220, 1024, 512, 'montanha');
+    fundo3 = game.add.tileSprite(0, 520, 1024, 256, 'caminho');
+    corredor = game.add.sprite(250, 580, 'correndo');
     corredor.animations.add('correr');
     corredor.animations.play('correr', 15, true);
     corredor.animations.pixelPerfect = true;
-    corredor.body.gravity.y = 10;
-    corredor.body.bounce.y = 0.4;
     corredor.body.collideWorldBounds = true;
     grupo = game.add.group();
+    qtde_moedas = 0;
 
     contador = 0;
     normal = false;
@@ -39,30 +45,48 @@ function create() {
         align: "center"
     });
     cursors = game.input.keyboard.createCursorKeys();
-    criar_ator(moedas, 'moeda', 2024, 580);
-    criar_ator(inimigos, 'inimigo', 1024, 580);
+    //criar_ator(moedas, 'moeda', 1024, 580);
+    criar_ator(inimigos, 'inimigo', 2024, 580);
+    
+    musica = game.add.audio('musica',1,true);
+    som_moeda = game.add.audio('moeda',1,true);
+    som_pulo = game.add.audio('pulo',1,true);
+    administrar_moedas();
+    //musica.play('',0,1,true);
+    game.stage.scale.startFullScreen();
 }
 
 function update() {
     fundo1.tilePosition.x -= 0.5;
     fundo2.tilePosition.x -= 1;
+    fundo3.tilePosition.x -= 2;
 
     corredor.body.velocity.x = 0;
 
     if (cursors.up.isDown)
     {
+        som_pulo.play();
         corredor.body.velocity.y = -400;
+        pos_atual += 200;
+        corredor.body.velocity.x = 200;
     }
-    atualiza_atores();
+    else if (pos_atual >= 0) {
+        corredor.body.velocity.x = -300;
+        pos_atual -= 300;
+    }
+    administra_atores(inimigos, 'inimigo');
+    administra_atores(moedas, 'moeda');
+    administrar_moedas();
     game.physics.collide(corredor, grupo, tratador_colisao, null, this);
 }
 
 function tratador_colisao(obj1, obj2) {
     if (obj2.name === 'moeda'){
+        som_pulo.play();
         obj2.kill();
         contador++;
+        qtde_moedas--;
         texto.setText("Moedas: " + contador);
-        criar_ator(moedas, 'moeda', 1024, 380);
     }
     else
     {
@@ -71,19 +95,35 @@ function tratador_colisao(obj1, obj2) {
     }
 }
 
+function administrar_moedas() {
+    console.log(qtde_moedas);
+    if (qtde_moedas <= 0) {
+        x = game.rnd.integerInRange(2, 10);
+        eixox = 1024;
+        for (i=0; i < x; i++) {
+            criar_ator(moedas, 'moeda', eixox, 300);
+            eixox+=50;
+            qtde_moedas++;
+        }
+    }
+}
+
 function criar_ator(grupo_ator, nome_ator, x, y) {
     var ator = grupo.create(x, y, nome_ator);
     ator.name = nome_ator;
     ator.animations.add('correr');
-    ator.animations.play('correr', 5, true);
+    ator.animations.play('correr', 10, true);
     ator.animations.pixelPerfect = true;
     grupo_ator[grupo_ator.length] = ator;
 }
 
-function atualiza_atores() {
-    for (i=0; i < inimigos.length; i++)
-        inimigos[i].body.velocity.x = -150;
-
-    for (i=0; i < moedas.length; i++)
-        moedas[i].body.velocity.x = -150;
+function administra_atores(grupo_ator, nome_ator) {
+    for (i=0; i < grupo_ator.length; i++) {
+        grupo_ator[i].body.velocity.x = -150;
+        if (grupo_ator[i].body.x < -150) {
+            grupo_ator[i].kill();
+            grupo_ator.splice(i, 1);
+            criar_ator(grupo_ator, nome_ator, 1024, 580);
+        }
+    }
 }
