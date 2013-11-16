29a30f95b94b7ce0cf74e059707586560ca4098d
CharlesGarrocho <charles.garrocho@gmail.com>
2013-11-16 11:42:21 -0200

    alterado: play de música para loop;

--- game_phaser/assets/js/Menu.js
+++ game_phaser/assets/js/Menu.js
@@ -1,6 +1,6 @@
 BasicGame.Menu = function (game) {
-	this.music = null;
-	this.playButton = null;
+	this.musica = null;
+	this.botao_jogar = null;
 };
 
 BasicGame.Menu.prototype = {
@@ -8,15 +8,15 @@ BasicGame.Menu.prototype = {
 	create: function () {
 	    this.fundo1 = this.add.tileSprite(0, 0, 1024, 512, 'nuvem');
 	    this.fundo2 = this.add.tileSprite(0, 512, 1024, 512, 'nuvem');
-		this.music = this.add.audio('musica');
-		this.music.play();
+		this.musica = this.add.audio('musica');
+		this.musica.play('',0,1,true);
 		this.estilo = { font: "bold 150pt Arial", fill: "#ffffff", align: "center", stroke: "#000000", strokeThickness: 15 };
 		this.estilo2 = { font: "bold 22pt Arial", fill: "#ffffff", align: "center", stroke: "#000000", strokeThickness: 5 };
         this.titulo = this.add.text(this.world.centerX, this.world.centerY-250, "JUMP!", this.estilo);
         this.titulo.anchor.setTo(0.5, 0.5);
         this.titulo2 = this.add.text(this.world.centerX, this.world.centerY-130, "Jogo Unificado para Movimentação Projetada", this.estilo2);
         this.titulo2.anchor.setTo(0.5, 0.5);
-		this.jogar = this.add.button(this.world.centerX - 150, 500, 'botao_jogar', this.startGame, this, 2, 1, 0);
+		this.botao_jogar = this.add.button(this.world.centerX - 150, 500, 'botao_jogar', this.novo_jogo, this, 2, 1, 0);
 	},
 
 	update: function () {
@@ -24,13 +24,8 @@ BasicGame.Menu.prototype = {
 	    this.fundo2.tilePosition.x -= 1;
 	},
 
-	startGame: function (pointer) {
-		this.music.stop();
+	novo_jogo: function (pointer) {
+		this.musica.stop();
 		this.game.state.start('Jogo');
 	},
-	
-	sair: function (pointer) {
-		this.music.stop();
-		this.game.state.start('Jogo');
-	}
 };
